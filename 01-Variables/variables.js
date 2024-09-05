afcc7b8b40a7feead8c8cdf1aa72f0b1e8142c09
Juanes Saavedra <juanessaavedra777@gmail.com>
2024-09-05 11:19:27 -0500

    add variables

---
+++ 01-Variables/variables.js
@@ -0,0 +1,23 @@
+/**  Variable = A container that stores a value. 
+ * Behaves as if it were the value it contains.
+ * 1. Declaration let x;
+ * 2. Assignment  x = 100;
+ * 
+ * let age = 25;
+let price = 10.99;
+let gpa = 2.1
+let firstName = "Juanes";
+let online = true;
+
+console.log(typeof firstName);
+console.log(`Juanes is online ${online}`);
+
+ * **/
+
+let fullName = "Juan Esteban";
+let age = 22;
+let student = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = student;
